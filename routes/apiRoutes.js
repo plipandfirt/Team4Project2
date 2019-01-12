24278c776fc3e82792eb0b7f2ed40e250e1a0c93
M Blydenburgh <mike.blydenburgh@gmail.com>
2019-01-12 10:55:28 -0500

    process env variables for edamam api url

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,9 @@
 const db = require("../models");
 const axios = require("axios");
-const appId = "9ba82081";
-const appKey = "7812af8b5126f42bf75451a05e092929";
+
+const appId = process.env.EDAMAM_RECIPE_APP_ID;
+const appKey = process.env.EDAMAM_RECIPE_APP_KEY;
+
 
 module.exports = function(app) {
   // Get all examples
