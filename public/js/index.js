3f602753fd7e65661fe49b06c063b36294e434fd
Green Man <brendan.mccaughey@gmail.com>
2019-01-13 21:15:29 -0500

    Some commenting on starter file.

--- public/js/index.js
+++ public/js/index.js
@@ -60,8 +60,8 @@ var refreshExamples = function() {
   });
 };
 
-handleFormSubmit is called whenever we submit a new example
-Save the new example to the db and refresh the list
+// handleFormSubmit is called whenever we submit a new example
+// Save the new example to the db and refresh the list
 var handleFormSubmit = function(event) {
   event.preventDefault();
 
@@ -83,8 +83,8 @@ var handleFormSubmit = function(event) {
   $exampleDescription.val("");
 };
 
-handleDeleteBtnClick is called when an example's delete button is clicked
-Remove the example from the db and refresh the list
+// handleDeleteBtnClick is called when an example's delete button is clicked
+// Remove the example from the db and refresh the list
 var handleDeleteBtnClick = function() {
   var idToDelete = $(this)
     .parent()
