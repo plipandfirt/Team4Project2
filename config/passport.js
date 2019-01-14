259d31c04b57b105572c0531c6143e75f2d4dae5
Michael Blydenburgh <mblydenburgh@icloud.com>
2019-01-14 19:02:27 +0000

    configure local auth route

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 /* eslint camelcase:0 */
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 const db = require("../models/index");
 
 // serializeUser taken a user after the findOrCreate method and creates a cookie to pass to the browser
@@ -42,8 +43,28 @@ passport.use(
         console.log(created);
         done(null,user);
       });
-
-
-
-  }
-  ));
+  }));
+  
+  passport.use(
+    new LocalStrategy(
+      (username, password, done) => {
+        db.user.findOne({
+          where:{
+            username:username,
+            password:passport
+          }
+        })
+        .then(user => {
+          if(user){
+            return done(null,user);
+          }
+          else{
+            return done(null,false);
+          }
+        })
+        .catch(err => {
+          return done(err);
+        });
+      }
+      )
+    );
