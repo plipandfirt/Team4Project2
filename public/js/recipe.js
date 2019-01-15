91483c290d857bff9108b7fc142564528d0ed460
Green Man <brendan.mccaughey@gmail.com>
2019-01-15 17:48:59 -0500

    Modified recipe.js to pull API response object into an array to be used to make cards.

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -8,93 +8,110 @@ const lastNameInput = document.querySelector("#last_name");
 const usernameInput = document.querySelector("#username");
 const passwordInput = document.querySelector("#password");
 const loginButton = document.querySelector("#login-button");
-<<<<<<< HEAD
-const loginModal = document.querySelector('#sign-up');
-const recipeModal = document.querySelector('#recipe-full');
-=======
 const nameFieldsDiv = document.querySelector(".name");
+const recipeModal = document.querySelector('#recipe-full');
+const receipeList = [];
 
->>>>>>> 18b4b7043ba2e0eff57fdcf1f87ba7eef86940b0
-
-document.addEventListener("DOMContentLoaded",(event)=>{
+document.addEventListener("DOMContentLoaded", (event) => {
   // console.log("recipe.js loaded");
-  
+
   //initialize materialize modals
   const loginModalInstance = M.Modal.init(loginModal);
   const recipeModalInstance = M.Modal.init(recipeModal);
-  
+
   //event listeners
-  loginModalButton.addEventListener("click",(event) => {
+  loginModalButton.addEventListener("click", (event) => {
     loginModalInstance.open();
   });
 
-  document.querySelector("#recipe-button").addEventListener("click",(event) => {
+  document.querySelector("#recipe-button").addEventListener("click", (event) => {
     recipeModalInstance.open();
   });
-  
-  loginButton.addEventListener("click",async (event) => {
+
+  loginButton.addEventListener("click", async (event) => {
     const username = usernameInput.value;
     const password = passwordInput.value;
     console.log(`logging in with ${username} & ${password}`);
-    await fetch("/login",{
-      method:"POST",
-      headers:{"Accept": "application/json",
-        "Content-Type": "application/json"},
-      body:JSON.stringify({"username":username,"password":password})
+    await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ "username": username, "password": password })
     });
   });
-  
+
   createAccountButton.addEventListener("click", async (event) => {
     // clicked state hold the logic for deciding whether to expand the login screen or take the form info and create a database entry
     let clickedState = event.target.dataset.clicked;
     console.log(`clicked: ${clickedState}`);
-    if(clickedState === "true"){ // if clicked, create database entry
+    if (clickedState === "true") { // if clicked, create database entry
       console.log("in clicked true");
       const data = {
-        firstName:firstNameInput.value,
-        lastName:lastNameInput.value,
-        username:usernameInput.value,
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value,
+        username: usernameInput.value,
         password: passwordInput.value
       };
-      
-      await fetch("/add",{
-        method:"POST",
+
+      await fetch("/add", {
+        method: "POST",
         body: JSON.stringify(data),
-        headers: {"Content-Type":"application/json"}
+        headers: { "Content-Type": "application/json" }
       })
         .then(res => {
           console.log(res);
-          createAccountButton.setAttribute("data-clicked","false");
+          createAccountButton.setAttribute("data-clicked", "false");
           nameFieldsDiv.classList.add("hide");
           loginModalInstance.close();
           loginButton.classList.remove("hide");
         })
         .catch(err => {
           console.log(err);
-          createAccountButton.setAttribute("data-clicked","false");
+          createAccountButton.setAttribute("data-clicked", "false");
           nameFieldsDiv.classList.add("hide");
         });
 
-      
+
     }
-    else{ // not clicked, expand the form and set clicked to true;
-      createAccountButton.setAttribute("data-clicked","true");
+    else { // not clicked, expand the form and set clicked to true;
+      createAccountButton.setAttribute("data-clicked", "true");
       nameFieldsDiv.classList.remove("hide");
       loginButton.classList.add("hide");
       createAccountButton.textContent = "Create Account";
     }
   });
-  
-  searchButton.addEventListener("click",(event) => {
+
+  //Searches for recipe and puts results into an array
+  searchButton.addEventListener("click", (event) => {
     console.log(searchInput.value);
-    fetchRecipes(searchInput.value).then(response=>console.log(response));
+    fetchRecipes(searchInput.value).then(response => {
+      const recipeList = [];
+      for (i = 0; i < response.data.length; i++) {
+        let newRecipe = {
+          label: response.data[i].recipe.label,
+          image: response.data[i].recipe.image,
+          ingredients: response.data[i].recipe.ingredients,
+          source: response.data[i].recipe.source,
+          url: response.data[i].recipe.url
+        }
+        recipeList.push(newRecipe);
+      }
+      console.log(recipeList);
+    });
   });
 
-  async function fetchRecipes(input){
+  async function fetchRecipes(input) {
     const query = input.split().join("&q=");
     console.log(`Running fetch recipes with ${query}`);
     const data = await fetch(`/api/recipes/${query}`);
     return data.json();
+  }
+
+  //create cards for each item in the recipeList
+  function makeCards() {
+
   }
 
 });
